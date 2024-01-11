6b42554e9ed2db2cebc05d3c2bff8ad80bc001fe
ahyng <ahyng1@gmail.com>
2024-01-11 11:18:48 +0900

    edit files

--- clock.js
+++ clock.js
@@ -4,13 +4,17 @@ const clock = function() {
     let month = date.getMonth();
     let d = date.getDate();
     let day = date.getDay();
+    if (d < 10) d = "0" + d;
 
     let hour = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
+    if (hour < 10) hour = "0" + hour;
+    if (minute < 10) minute = "0" + minute;
+    if (second < 10) second = "0" + second;
 
-    let today = month+1 + "." + d + "." + getday[day];
+    let today = month+1 + "." + d + " " + getday[day];
 
-    document.getElementById("clock").innerHTML = today + " " + hour + ":" + minute + ":" + second;
+    document.getElementById("clock").innerHTML = today + "<br>" + hour + ":" + minute + ":" + second;
 }
 setInterval(clock, 1000);

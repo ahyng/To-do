113d29eb0e158c9edce51291a3031a44048959f4
ahyng <ahyng1@gmail.com>
2024-01-11 14:39:19 +0900

    add list and button size

--- todo.js
+++ todo.js
@@ -3,8 +3,16 @@ let input = document.getElementById('input');
 let lst = document.getElementById('lst');
 
 btn.addEventListener("click", function add() {
+    if (input.value == ''){
+        alert("내용을 입력해 주세요.")
+    } else {
     let create = document.createElement("li");
-    create.innerHTML = input.value;
+    let del = document.createElement("button");
+    del.style.fontSize="40px";
+    create.innerHTML = input.value + " ";
+    del.innerHTML = "X";
     lst.appendChild(create);
+    create.appendChild(del);
     input.value = "";
+    }
 })

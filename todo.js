19558e22835429ebcd67ed1a0cd2a9642f0ad853
ahyng <ahyng1@gmail.com>
2024-01-11 11:33:35 +0900

    add todo-list

--- todo.js
+++ todo.js
@@ -1,1 +1,10 @@
-console.log("hi")
+let btn = document.getElementById('btn');
+let input = document.getElementById('input');
+let lst = document.getElementById('lst');
+
+btn.addEventListener("click", function add() {
+    let create = document.createElement("li");
+    create.innerHTML = input.value;
+    lst.appendChild(create);
+    input.value = "";
+})
